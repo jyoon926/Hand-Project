7fac66f2d5dcd60219d2c2630acf65d178277394
Jacob Yoon <jacobyoon926@gmail.com>
2020-12-18 09:58:07 -0500

    Final

--- script.js
+++ script.js
@@ -1,61 +1,176 @@
-canvas = document.getElementById("canvas");
-context = canvas.getContext("2d");
-log = document.getElementById('log');
-//Width of image in pixels
-width = canvas.offsetHeight;
-//Actual width of image in cm
-const rawWidth = 21.575;
-//Square cm per pixel of the image
-factor = (rawWidth / width)**2;
-image = new Image();
+// Initialize DOM elements
 
-function start() {
-    image.src = 'img/Hand Solid ' + width + '.png';
-    image.onload = function(){
-        context.drawImage(image, 0, 0, width, width);
+// "VFX" ==> scanning effects, "FTX" ==> actual hand image
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d')
+const effects= document.getElementById('effects');
+const vfx = effects.getContext('2d')
+const CalcButton = document.getElementById('calculate')
+const LogBox = document.getElementById('log')
+CalcButton.onclick = Calculate
+
+// Variables
+let image = new Image()
+let center, ratio, rawWidth, factor, display;
+let width = Apply()
+let ScanHeight = 0
+let Scanning = false
+let RESULT;
+
+
+/*
+
+FUNCTIONS
+    Resize(): Resize canvas according to screen size
+    Log(): Display results in box
+    Apply(): Change hand & size values
+    Calculate(): Calculate results & display
+
+*/
+
+function Resize(){
+    width = parseInt(Apply())
+    ratio = (display/width)**2
+    canvas.width = window.innerWidth/2
+    canvas.height = window.innerWidth/2
+    effects.width = canvas.width
+    effects.height = canvas.height
+    center = canvas.width/2
+    display = canvas.width*0.7
+
+    switch(document.getElementById("hand").value){
+        case 'jacob':
+            rawWidth = 19.6;
+            break;
+        case 'benigno':
+            rawWidth = 21.0;
+            break;
+        default:
+            rawWidth = 20;
+            break;
     }
+
+    factor = (rawWidth / width)**2;
 }
 
-function compute() {var imgData = context.getImageData(0, 0, width, width);
+function Log(arg){
+    LogBox.innerHTML = arg
+}
+
+function Apply(){
+
+    // Set Size
+    let sizeSelect = document.getElementById("size");
+    let sizeValue = sizeSelect.value
+
+    // Set Hand
+    let handSelect = document.getElementById("hand");
+    let handValue = handSelect.value
+
+    image.src = `img/${handValue}-${sizeValue}.png`
+
+    return sizeValue;
+}
+
+function Calculate() {
+
+    Scanning = true
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.width);
     var data = imgData.data;
     var sum = 0;
 
-    for (var i = 0; i < data.length; i += 4) {
-        const alpha = data[i + 3];
+    for (let i = 0; i < data.length; i += 4) {
+        let alpha = data[i + 3];
         if (alpha > 100) {
             sum++;
         }
+
     }
-    print(
-        "Image Width = " + width + " px" + "<br />"
-        + "Actual Width = " + Math.sqrt(width**2 * factor) + " cm" + "<br />"
-        + "1 px = " + factor.toFixed(6) + " cm^2" + "<br />"
-        + "Image Area = " + width**2 + " px" + "<br />"
-        + "Hand Area = " + sum + " px" + "<br /><br />"
-        + "Hand Area = " + (sum * factor).toFixed(3) + " cm^2" + "<br />"
-        + "Hand Area = " + (sum * factor / 6.452).toFixed(3) + " in^2"
-    );
+
+
+    Log
+    (
+        `Calculating...`
+    )
+
+    RESULT =
+    `
+        Image Width: <span> ${rawWidth} cm </span> <br>
+        Image Width: <span> ${width} px </span> <br>
+        <br>
+        Pixel Density: <span> ${(factor).toFixed(3)} cm<sup>2</sup>/px </span> <br>
+        <br>
+        Display Width: <span> ${display.toFixed(3)} px </span> <br>
+        Scale Factor: <span> ${ratio.toFixed(3)} </span> <br>
+        <br>
+        Hand Area: <span> ${(sum/ratio).toFixed(0)} px</span> <br>
+        Hand Area: <span> ${(sum*factor/ratio/6.4516).toFixed(3)} in<sup>2</sup> <span> <br>
+        <br>
+        Hand Area: <span class = 'gold'> ${(sum*factor/ratio).toFixed(3)} cm<sup>2</sup> <span> <br>
+    `
+
 }
 
-function print(str) {
-    log.innerHTML = str;
-}
-function display(str) {
-    log.innerHTML = str + '<br />' + log.innerHTML;
-}
-function line() {
-    log.innerHTML = '----------<br />' + log.innerHTML;
+/*
+
+ANIMATION LOOP
+Some funky stuff going on here
+Would recommend staying away if possible
+
+*/
+
+const loop = () => {
+
+    Resize()
+
+    if (Scanning){
+
+        ScanHeight += canvas.width/100
+
+        vfx.strokeStyle = 'red'
+        vfx.lineWidth = 5
+        vfx.beginPath()
+        vfx.moveTo(0, canvas.width-ScanHeight)
+        vfx.lineTo(canvas.width, canvas.width-ScanHeight)
+        vfx.closePath()
+        vfx.stroke()
+
+        vfx.globalAlpha = 0.1
+        vfx.fillStyle = 'blue'
+        vfx.fillRect(center-display/2, center-display/2, display, display)
+        vfx.globalAlpha = 1
+
+        if (ScanHeight > canvas.width){
+            Scanning = false;
+            Log(RESULT)
+            setTimeout(()=>{
+                ScanHeight = 0
+            },500)
+        }
+
+        vfx.globalAlpha = 0.3
+        vfx.fillStyle = 'red'
+        if (canvas.width-ScanHeight < center+display/2 && canvas.width-ScanHeight > center-display/2){
+            vfx.fillRect(center-display/2, canvas.width-ScanHeight, display, (center+display/2)-(canvas.width-ScanHeight))
+
+        }
+
+        if (canvas.width-ScanHeight < center-display/2){
+            vfx.fillRect(center-display/2, center-display/2, display, display)
+        }
+
+        vfx.globalAlpha = 1
+    }
+
+    //ctx.drawImage(image, center-width*ratio/2, center-width*ratio/2, width*ratio, width*ratio)
+
+ //   ctx.drawImage(image, center/2, center/2, center, center)
+
+    ctx.imageSmoothingEnabled = false;
+    ctx.drawImage(image, center-display/2, center-display/2, display, display)
+
+    window.requestAnimationFrame(loop)
 }
 
-function SetSize() {
-    var e = document.getElementById("size");
-    var s = parseInt(e.options[e.selectedIndex].text);
-    document.getElementById("canvas").width = s;
-    document.getElementById("canvas").height = s;
-    width = s;
-    factor = (rawWidth / width)**2;
-    image.src = 'img/Hand Solid ' + width + '.png';
-    image.onload = function(){
-        context.drawImage(image, 0, 0, width, width);
-    }
-}
+window.requestAnimationFrame(loop)
+
